c594f1791b19ffcfbccad6598f71f0fda497e075
zweizeichen <info@zweizeichen.org>
2013-06-20 12:20:53 +0200

    Added tree autoloading

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -106,12 +106,6 @@ $.getJSON('/javascripts/groups.json', function(groups) {
 			duration: 200
 		},
 		children: groups,
-		// Animation
-		onLazyRead: function(node) {
-			node.appendAjax({
-				url: "/market/browse/tree/" + node.data.key + "/"
-			}); // AJAX URL
-		},
 		onActivate: function(node) {
 			if(!node.data.isFolder) {
 
@@ -144,26 +138,8 @@ $.getJSON('/javascripts/groups.json', function(groups) {
 			}
 		},
 		onPostInit: function(isReloading, isError) {
-			// If load_path defined load path
-			if(typeof load_path !== 'undefined') {
-				this.loadKeyPath(load_path, function(node, status) {
-					if(status == "loaded") {
-						// 'node' is a parent that was just traversed.
-						// If we call expand() here, then all nodes will be expanded
-						// as we go
-						node.expand();
-					} else if(status == "ok") {
-						// 'node' is the end node of our path.
-						// If we call activate() or makeVisible() here, then the
-						// whole branch will be exoanded now
-						node.activate();
-					} else if(status == "notfound") {
-						var seg = arguments[2],
-							isEndNode = arguments[3];
-					}
-				});
-
-			}
+			// Auto-activate minerals
+			this.activateKey(18);
 		}
 	});
 });
